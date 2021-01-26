88bc36fa0a90ac07d56d67d79171099d75b53326
Bennett <bennettlee908@gmail.com>
2021-01-25 20:29:16 -0800

    first commit

---
+++ Week2/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Lee Bennett on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 20 x 20 points
+    // 2x - 40 x 40 pixels
+    // 3x - 60 x 60 pixels
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
