b784adb4da3a1d1b290396078c0e509568ad3d17
Bennett <bennettlee908@gmail.com>
2021-01-28 09:37:34 -0800

    .

---
+++ Week2/AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Lee Bennett on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
