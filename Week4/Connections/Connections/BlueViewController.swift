3b5b97609b22eedaf92daf40373d018dc46b4887
Bennett <bennettlee908@gmail.com>
2021-02-11 09:36:09 -0800

    updated week 4

---
+++ Week4/Connections/Connections/BlueViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BlueViewController.swift
+//  Connections
+//
+//  Created by Lee Bennett on 2/10/21.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\(#function)")
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Week4/Connections/Connections/ViewController.swift
+++ Week4/Connections/Connections/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        nameTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backgroundDidTapped(_ sender: UIButton) {
+        nameTextField.resignFirstResponder()
+    }
+    
+    @IBAction func userDidTappedReturn(_ sender: UITextField) {
+        nameTextField.resignFirstResponder()
+    }
+    
     @IBAction func buttonDidPressed(_ sender: UIButton) {
+        let name: String = nameTextField.text ?? ""
+        
         mainLabel.text = "Hello"
     }
     
